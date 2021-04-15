6a459b0298629a51211a906e13e0776cc152baf9
aPlacuzzi <andrea.placuzzi@unibo.it>
2021-04-15 16:47:58 +0200

    add assert to verify that cloneNode method works

--- src/main/scala/it/unibo/blocks/Clone.scala
+++ src/main/scala/it/unibo/blocks/Clone.scala
@@ -7,18 +7,18 @@ import it.unibo.casestudy.CrowdEstimationLib
 class Clone extends AggregateProgram  with StandardSensors with BlockG with CrowdEstimationLib with ScafiAlchemistSupport
   with CustomSpawn {
   override def main(): Any = {
+    val myNode = alchemistEnvironment.getNodeByID(mid())
+    val myPos = alchemistEnvironment.getPosition(myNode)
     rep(init = true) {
       case true => {
-        val myNode = alchemistEnvironment.getNodeByID(mid())
         val clone = myNode.cloneNode(alchemistTimestamp)
-        val myPos = alchemistEnvironment.getPosition(myNode)
         val gpsPoint = new LatLongPosition(myPos.getCoordinate(0) + 0.0001, myPos.getCoordinate(1) + 0.0001)
         alchemistEnvironment.addNode(clone, gpsPoint)
         false
       }
       case _ => false
     }
-
+    assert(alchemistEnvironment.getNodes.stream().map { n => alchemistEnvironment.getPosition(n) }.filter { pos => pos.equals(myPos) }.count() == 1)
     true
   }
 }

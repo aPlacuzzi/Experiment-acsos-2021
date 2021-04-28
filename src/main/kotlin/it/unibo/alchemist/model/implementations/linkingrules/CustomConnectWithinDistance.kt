4be0704f187fb75b58de4770dbe9b0911381ad00
aPlacuzzi <andrea.placuzzi@unibo.it>
2021-04-28 15:44:51 +0200

    use linkingRule with CustomConnectWithinDistance

--- src/main/kotlin/it/unibo/alchemist/model/implementations/linkingrules/CustomConnectWithinDistance.kt
+++ src/main/kotlin/it/unibo/alchemist/model/implementations/linkingrules/CustomConnectWithinDistance.kt
@@ -4,21 +4,25 @@ import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
 import it.unibo.alchemist.model.implementations.neighborhoods.Neighborhoods
 import it.unibo.alchemist.model.interfaces.*
 
-open class CustomConnectWithinDistance<T, P : Position<P>>(val radius: Double, val accessPointRadius: Double, val accessPointMolecule: Molecule)
+open class CustomConnectWithinDistance<T, P : Position<P>>(val radius: Double, val virtualRadius: Double, virtualMoleculeKey: String)
     : LinkingRule<T,P> {
-    override fun isLocallyConsistent(): Boolean = false
 
-    private val Node<T>.isAccessPoint
-        get() = getConcentration(accessPointMolecule) as Boolean
+    val virtualMolecule: Molecule = SimpleMolecule(virtualMoleculeKey)
+
+    override fun isLocallyConsistent(): Boolean = true
+
+    private val Node<T>.isVirtualNode
+        get() = getConcentration(virtualMolecule) as Boolean
 
     override fun computeNeighborhood(center: Node<T>, env: Environment<T, P>): Neighborhood<T> {
         val nbrs = env.getNodesWithinRange(center, radius)
-        val nextNbrs = env.getNodesWithinRange(center, accessPointRadius)
-        return Neighborhoods.make(env, center,
-            if(center.isAccessPoint) {
-                nbrs.addAll(nextNbrs.filter { it.isAccessPoint })
-                nbrs
-            } else nbrs
-        )
+        val nextNbrs = env.getNodesWithinRange(center, virtualRadius)
+        if (center.isVirtualNode) {
+            return Neighborhoods.make(env, center, nextNbrs)
+        } else {
+            nbrs.addAll(nextNbrs.filter { it.isVirtualNode })
+            return Neighborhoods.make(env, center, nbrs)
+        }
     }
+
 }

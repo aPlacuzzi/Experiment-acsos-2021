d571c499d1e21c41b71150c921ce4049c1a78dac
aPlacuzzi <andrea.placuzzi@unibo.it>
2021-05-01 17:47:12 +0200

    add vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     implementation("it.unibo.alchemist:alchemist:_")
     implementation("it.unibo.alchemist:alchemist-maps:_")
-//    implementation("it.unibo.alchemist:alchemist-swingui:_")
+    implementation("it.unibo.alchemist:alchemist-swingui:_")
     implementation("it.unibo.alchemist:alchemist-incarnation-protelis:_")
     implementation("it.unibo.alchemist:alchemist-incarnation-scafi:_")
     implementation("org.scala-lang:scala-library:2.13.4")
@@ -118,8 +118,7 @@ fun makeTest(
             "-t", "$time",
             "-e", "$outputDir/$datafilename",
             "-p", threadCount,
-            "-i", "$sampling",
-            "-hl"
+            "-i", "$sampling"
         )
         if (vars.isNotEmpty()) {
             args("-b", "-var", *vars.toTypedArray())
@@ -143,6 +142,6 @@ createTask("crowdWithVirtuals", "crowdWithVirtuals", "caseStudy", "json")
 createTask("runScafi", "crowdWarningScafi", "crowd")
 createTask("runProtelis", "crowdWarningProtelis", "crowd")
 makeTest(name="batch", file = "crowdWithVirtuals", time = 700.0, vars = setOf("random"), taskSize = 1500, threads = 1, sampling = 10.0)
-makeTest(name="batch1", file = "crowdWithVirtuals1", time = 700.0, taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data1")
-makeTest(name="batch2", file = "crowdWithVirtuals2", time = 700.0, taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data2")
-makeTest(name="batch3", file = "crowdWithVirtuals3", time = 700.0, taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data3")
+makeTest(name="batch1", file = "crowdWithVirtuals1", time = 700.0, vars = setOf("random"), taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data1")
+makeTest(name="batch2", file = "crowdWithVirtuals2", time = 700.0, vars = setOf("random"), taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data2")
+makeTest(name="batch3", file = "crowdWithVirtuals3", time = 700.0, vars = setOf("random"), taskSize = 1500, threads = 1, sampling = 10.0, outputDir = "data3", effects = "caseStudy.json")

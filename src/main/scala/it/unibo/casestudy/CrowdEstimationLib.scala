ca49f394dea3876735c2899dcf7266b854944b97
aPlacuzzi <andrea.placuzzi@unibo.it>
2021-05-13 10:14:54 +0200

    remove unused code

--- src/main/scala/it/unibo/casestudy/CrowdEstimationLib.scala
+++ src/main/scala/it/unibo/casestudy/CrowdEstimationLib.scala
@@ -1,10 +1,9 @@
 package it.unibo.casestudy
 
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
-import it.unibo.scafi.space.{Point2D, Point3D}
 
 import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.{DAYS, Duration, FiniteDuration}
+import scala.concurrent.duration.FiniteDuration
 
 /**
  * See papers:
@@ -21,29 +20,11 @@ trait CrowdEstimationLib extends BuildingBlocks {
    * the app (about 0.5 percent of marathon attendees), and w estimates the
    * fraction of walkable space in the local urban environment.
    */
-    // X ROBY, mi confermi che con questa foldhood conta i soli vicini umani (e non i virtuali) all'interno di range
   def countNearby(range: Double): Double = foldhood(0)(_ + _)(mux(nbr(node.get("human").asInstanceOf[Boolean]) & nbrRange() < range) { 1 } { 0 })
 
   def densityEstimation(p: Double, range: Double, w: Double): Double = countNearby(range) / (p * Math.PI * Math.pow(range, 2) * w)
 
   def isRecentEvent(event: Boolean, timeout: Double): Boolean = recentlyTrue(FiniteDuration(timeout.toLong, TimeUnit.SECONDS), event)
-
-  /**
-   * def dangerousDensity(p, range, dangerousDensity, groupSize, w) {
-   * let partition = S(range, nbrRange);
-   * let localDensity = densityEstimation(p, range, w);
-   * let avg = summarize(partition, sum, localDensity, 0) / summarize(partition, sum, 1, 0);
-   * let count = summarize(partition, sum, 1 / p, 0);
-   * avg > dangerousDensity && count > groupSize
-   * }
-   */
-  def dangerousDensityFull(p: Double, range: Double, dangerousDensity: Double, groupSize: Double, w: Double): Boolean = {
-    val partition = myS2(range, nbrRange)
-    val localDensity = densityEstimation(p, range, w)
-    val avg = summarize(partition, _ + _, localDensity, 0.0) / summarize(partition, _ + _, 1.0, 0.0)
-    val count = summarize(partition, _ + _, 1.0 / p, 0.0)
-    broadcast(partition, avg > dangerousDensity && count > groupSize) // X ROBY: la broadcast qui serve?
-  }
 
   /**
    * def crowdTracking(p, range, w, crowdedDensity, dangerousThreshold, groupSize, timeFrame) {
@@ -74,9 +55,6 @@ trait CrowdEstimationLib extends BuildingBlocks {
     node.put("_near", countNearby(range))
     val densityEst = densityEstimation(p, range, w)
     branch (isRecentEvent(densityEst > crowdedDensity, timeFrame)) {
-      /*branch (dangerousDensityFull(p, range, dangerousThreshold, groupSize, w)) { // Ignore difference between Overcrowded and atRisk
-        Overcrowded.asInstanceOf[Crowding]
-      } { AtRisk.asInstanceOf[Crowding] }*/
       AtRisk.asInstanceOf[Crowding]
     } {
       Fine.asInstanceOf[Crowding]
@@ -92,36 +70,4 @@ trait CrowdEstimationLib extends BuildingBlocks {
   case object AtRisk extends Crowding
 
   case object Fine extends Crowding
-
-  val noAdvice = Point2D(Double.NaN, Double.NaN)
-
-  val inf: (Double, ID) = (Double.PositiveInfinity, Int.MaxValue)
-
-  def myS2(grain: Double, metric: Metric) = myBreakUsingUids(randomUid, grain, metric)
-
-  def myBreakUsingUids(
-    uid: (Double, ID),
-    grain: Double,
-    metric: Metric
-  ): Boolean =
-    share(uid) { case (lead, nbrLead) =>
-      myDistanceCompetition2(distanceTo(lead == uid, metric), nbrLead(), uid, grain)
-    } == uid
-
-  def myDistanceCompetition2(distance: Double, nbrLead: (Double, ID), uid: (Double, ID), grain: Double): (Double, ID) = {
-    mux (distance > grain) {
-      uid
-    } {
-      val thr = 0.25 * grain;
-      mux (distance >= thr) {
-        inf
-      } {
-        mux (distance >= thr) {
-          inf
-        } {
-          minHood(nbr(nbrLead))
-        }
-      }
-    }
-  }
 }
